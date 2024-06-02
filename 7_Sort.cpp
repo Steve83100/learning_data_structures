47ad05931f5a3514883fb64756bc887cc8639d99
Steve <sy9666216@gmail.com>
2024-06-02 20:36:55 +0800

    Add merge sort, heap sort

--- 7_Sort.cpp
+++ 7_Sort.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#define maxNum 100
 using namespace std;
 
 template <typename Type>
@@ -52,6 +53,8 @@ void selectSort(Type nums[], int size){
     }
 }
 
+
+
 template <typename Type>
 void insertSort4Shell(Type nums[], int size, int start, int step){
     // 可设置步长的插入排序，为希尔排序使用；传入的数组仍然是完整的原数组
@@ -77,8 +80,10 @@ void shellSort(Type nums[], int size){
     insertSort4Shell(nums, size, 0, 1); // 最后对全体数组进行插入排序
 }
 
+
+
 template <typename Type>
-void quickSort(Type nums[], int left, int right){
+void QSort(Type nums[], int left, int right){
     // 快速排序，根据两个位置参数完成局部排序
     if(left < right){
         Type pivot = nums[left]; // 选最左侧元素为参照
@@ -93,10 +98,90 @@ void quickSort(Type nums[], int left, int right){
         }
 
         nums[low] = pivot; // 此时low==high，已满足左小右大的关系，将pivot填入low位置即可
-        quickSort(nums, left, low-1); // 对左边进行排序
-        quickSort(nums, low+1, right); // 对右边进行排序
+        QSort(nums, left, low-1); // 对左边进行排序
+        QSort(nums, low+1, right); // 对右边进行排序
     }
 }
+
+template <typename Type>
+void quickSort(Type nums[], int size){
+    QSort(nums, 0, size-1);
+}
+
+
+
+template <typename Type>
+void MSort(Type nums[], Type res[], int start, int end){
+    // 将nums数组中从start到end的元素排序，存放在res数组中
+    if(start == end) res[start] = nums[start]; // 只有一个元素，直接填入对应位置即可
+    else{ // 有多个元素
+        int mid = (start + end) / 2; // 取中间位置
+        Type part[maxNum]; // 辅助数组，用于存储排好序的部分
+
+        MSort(nums, part, start, mid); // 对两部分分别进行归并排序，分别存储在part数组的前半段和后半段
+        MSort(nums, part, mid+1, end);
+
+        // 对两部分分别排序后，接下来需要将它们合并
+        int i = start, j = mid+1, k = start;
+        while(i <= mid && j <= end){
+            if(part[i] < part[j]){
+                res[k] = part[i];
+                i++;
+            }
+            else{
+                res[k] = part[j];
+                j++;
+            }
+            k++;
+        }
+        while(i <= mid){
+            res[k] = part[i];
+            i++; k++;
+        }
+        while(j <= end){
+            res[k] = part[j];
+            j++; k++;
+        }
+    }
+}
+
+template <typename Type>
+void mergeSort(Type nums[], int size){
+    MSort(nums, nums, 0, size-1);
+}
+
+
+
+template <typename Type>
+void filterDown(Type heap[], int size, int root){
+    // 对堆中的节点root，假定其左右子树均为大顶堆，将以root为根节点的子树调整为大顶堆
+    int currentPos = root;
+    int currentChild = currentPos * 2 + 1;
+    Type temp = heap[root]; // 存储根节点
+    while(currentChild < size){ // 不断向下寻找root该去的位置
+        if(currentChild+1 < size && heap[currentChild] < heap[currentChild+1]) currentChild++;
+        // 将currentChild指向两个子节点中更大的那个
+        if(temp >= heap[currentChild]) break; // 若root比这两个子节点中更大的还大，说明已经满足大顶堆条件
+
+        heap[currentPos] = heap[currentChild]; // 否则，将这个更大的子节点上提
+        currentPos = currentChild; // 将考察的位置下移至那个更大的子节点
+        currentChild = currentPos * 2 + 1;
+    }
+    heap[currentPos] = temp; // 已经找到root该去的地方，将其填入
+}
+
+template <typename Type>
+void heapSort(Type heap[], int n){
+    for(int i = n/2-1; i >= 0; i--) filterDown(heap, n, i); // 将数组初始化为大顶堆
+    for(int i = n-1; i > 0; i--){
+        Type temp = heap[i]; // 将堆顶最大的元素交换到数组末端，后续将不再考虑该元素
+        heap[i] = heap[0];
+        heap[0] = temp;
+        filterDown(heap, i, 0); // 重新调整大顶堆；由于仅修改了堆顶元素，故只需对堆顶元素进行调整即可
+    }
+}
+
+
 
 int main(){
     int num1[15] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
@@ -114,9 +199,15 @@ int main(){
     // shellSort<int>(num1, 15);
     // shellSort<int>(num2, 15);
     // shellSort<int>(num3, 15);
-    quickSort<int>(num1, 0, 14);
-    quickSort<int>(num2, 0, 14);
-    quickSort<int>(num3, 0, 14);
+    // quickSort<int>(num1, 15);
+    // quickSort<int>(num2, 15);
+    // quickSort<int>(num3, 15);
+    // mergeSort<int>(num1, 15);
+    // mergeSort<int>(num2, 15);
+    // mergeSort<int>(num3, 15);
+    heapSort<int>(num1, 15);
+    heapSort<int>(num2, 15);
+    heapSort<int>(num3, 15);
     for(int i = 0; i < 15; i++){
         cout<<num1[i]<<' ';
     }
